1a97ee47a212e44764a35be377160ea722ec6b05
cruiseqe2 <cruiseqe2@gmail.com>
2024-08-07 22:20:23 +0100

    Initial Save - First Version

---
+++ RoseAndCrown/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  RoseAndCrown
+//
+//  Created by Mark Oelbaum on 07/08/2024.
+//
+
+import Foundation
---
+++ RoseAndCrown/ViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  RCModel.swift
+//  RoseAndCrown
+//
+//  Created by Mark Oelbaum on 07/08/2024.
+//
+
+import Foundation
+
+@Observable
+class ViewModel {
+    
+    var items: [Item] = []
+    
+    init(items: [Item]) {
+    
+    }
+    
+    
+    
+    
+}
