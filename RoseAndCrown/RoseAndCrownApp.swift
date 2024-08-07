06e6e99db46a4f5011fae8eddcfd54a27e9de26b
cruiseqe2 <cruiseqe2@gmail.com>
2024-08-07 01:07:15 +0100

    Initial Commit

---
+++ RoseAndCrown/RoseAndCrownApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoseAndCrownApp.swift
+//  RoseAndCrown
+//
+//  Created by Mark Oelbaum on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RoseAndCrownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
